3946d3bedb4912031c2afafd1fe8ad9c96b7238d
elliIMA <wiz33@yandex.ru>
2022-11-12 16:58:05 +0300

    added redirect in calculate geometry app

--- my_page/geometry/urls.py
+++ my_page/geometry/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('rectangle/<int:width>/<int:height>', get_rectangle_area),
-    path('square/<int:width>/', get_square_area),
-    path('circle/<int:radius>/', get_circle_area),
-
+    path('rectangle/<int:width>/<int:height>', rectangle),
+    path('square/<int:width>/', square),
+    path('circle/<int:radius>/', circle),
+    path('get_rectangle_area/<int:width>/<int:height>', get_rectangle_area),
+    path('get_square_area/<int:width>/', get_square_area),
+    path('get_circle_area/<int:radius>/', get_circle_area),
 ]
--- my_page/geometry/views.py
+++ my_page/geometry/views.py
@@ -1,14 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def get_rectangle_area(request, width: int, height: int):
-    return HttpResponse(
-        f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
-
-
+    return HttpResponseRedirect(f'/calculate_geometry/rectangle/{width}/{height}')
 def get_square_area(requset, width: int):
-    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width ** 2}')
+    return HttpResponseRedirect(f'/calculate_geometry/square/{width}')
 
 
 def get_circle_area(request, radius: int):
+    return HttpResponseRedirect(f'/calculate_geometry/circle/{radius}')
+
+
+def rectangle(request, width: int, height: int):
+    return HttpResponse(
+        f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
+
+def square(requset, width: int):
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width ** 2}')
+
+def circle(request, radius: int):
     return HttpResponse(f'Площадь круга радиусом {radius} равна {3.14 * radius**2}')
